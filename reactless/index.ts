44ff7de326c8e142a9a9c199883f2f6f24482e8c
GeneCodeSavvy <hxrsh.09@gmail.com>
2025-10-02 17:22:35 +0530

    make the syntax more react like

--- reactless/index.ts
+++ reactless/index.ts
@@ -1,10 +1,12 @@
-import { render } from './react-dom';
-import App from './app';
+import { render, createElement } from './vdom';
+import App from '../src/app';
 
 const rootElement = document.getElementById('root');
 
+const app = App()
+
 if (rootElement) {
-    render(rootElement, App);
+    render(rootElement, app);
 } else {
     console.error('Root element not found');
 }
---
+++ src/app.tsx
@@ -0,0 +1,3 @@
+const App = () => <h1 title="harsh" className="main">Hello World From the Browser!</h1>;
+
+export default App;
